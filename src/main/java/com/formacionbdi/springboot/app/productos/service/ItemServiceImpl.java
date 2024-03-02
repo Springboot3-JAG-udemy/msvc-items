6be40a30089215c33b7dbb3ad0bd3e761b9126ed
AngelTravieso <angeltraviesoc@gmail.com>
2024-03-02 16:05:45 -0400

    Implementando componente service con cliente HTTP RestTemplate

--- src/main/java/com/formacionbdi/springboot/app/productos/service/ItemServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/service/ItemServiceImpl.java
@@ -1,11 +1,13 @@
 package com.formacionbdi.springboot.app.productos.service;
 
 import com.formacionbdi.springboot.app.productos.models.Item;
+import com.formacionbdi.springboot.app.productos.models.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -15,11 +17,20 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public List<Item> findAll() {
-        return null;
+
+        List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:4250/listar", Producto[].class));
+
+        return productos.stream().map(producto -> new Item(producto, 1)).collect(Collectors.toList());
     }
 
     @Override
-    public Item findById(Long Id, Integer cantidad) {
-        return null;
+    public Item findById(Long id, Integer cantidad) {
+
+        Map<String, String> pathVariables = new HashMap<String, String>();
+        pathVariables.put("id", id.toString());
+
+        Producto producto = clienteRest.getForObject("http://localhost:4251/listar/${id}", Producto.class, pathVariables);
+
+        return new Item(producto, cantidad);
     }
 }
