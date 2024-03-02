092987537cd9894741a1183dd791eb5ea3ff57f0
AngelTravieso <angeltraviesoc@gmail.com>
2024-03-02 15:58:27 -0400

    Creando componente service en items para consumir API productos

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/AppConfig.java
@@ -0,0 +1,15 @@
+package com.formacionbdi.springboot.app.productos;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean("clienteRest")
+    public RestTemplate regisrarRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
---
+++ src/main/java/com/formacionbdi/springboot/app/productos/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.formacionbdi.springboot.app.productos.service;
+
+import com.formacionbdi.springboot.app.productos.models.Item;
+
+import java.util.List;
+
+public interface ItemService {
+    public List<Item> findAll();
+    public Item findById(Long Id, Integer cantidad);
+}
