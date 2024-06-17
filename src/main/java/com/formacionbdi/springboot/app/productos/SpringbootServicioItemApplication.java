d79285496ebc994e350e98c276e0afb22cc4e5d3
AngelTravieso <angeltraviesoc@gmail.com>
2024-06-16 20:11:56 -0400

    Tolerancia de fallos y latencia con Hystrix

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioItemApplication.java
@@ -2,10 +2,13 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 // import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// Habilitar hystrix
+@EnableCircuitBreaker
 // No es obligatorio, pero es recomendable habilitarla de manera explicita
 @EnableEurekaClient
 // Se quita si se usa eureka client, ya que esta ya treae ribbon embebido
--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ItemController.java
@@ -1,7 +1,10 @@
 package com.formacionbdi.springboot.app.productos.controllers;
 
 import com.formacionbdi.springboot.app.productos.models.Item;
+import com.formacionbdi.springboot.app.productos.models.Producto;
 import com.formacionbdi.springboot.app.productos.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +26,27 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    
+    // En caso de falla del servicio, llamamos otro metodo
+    @HystrixCommand(fallbackMethod = "metodoAlternativo")
     @GetMapping("/listar/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
         return itemService.findById(id, cantidad);
+    }
+
+    public Item metodoAlternativo(Long id, Integer cantidad) {
+
+        Item item = new Item();
+        Producto producto = new Producto();
+
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+
+        return item;
+
     }
 
 }
